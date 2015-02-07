56c656e921f548ea741cc99f39595614982223a7
mikeking27 <mking@ostusa.com>
2015-02-07 13:12:45 -0500

    menu stuff

---
+++ Assets/LoadingScreen/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour
+{
+
+    public void OnClickLevel1()
+    {
+        Application.LoadLevel("level_one");
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
